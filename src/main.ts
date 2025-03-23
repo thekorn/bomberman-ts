a408fad27f9b75234b550cd9f2ded3486073e600
Markus Korn <markus.korn@gmail.com>
2025-03-23 07:38:56 +0100

    Standardize string quotes and update editor settings
    - Standardize code style by converting double quotes to single quotes across TypeScript files.
    - Update biome configuration to allow the use of an ignore file.
    - Enable automated code formatting on save in Zed editor settings.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { setupGame } from "./game.ts";
-import "./style.css";
-import { assert } from "./utils.ts";
+import { setupGame } from './game.ts';
+import './style.css';
+import { assert } from './utils.ts';
 
 const GridCols = 23;
 const GridRows = 12;
 
-const app = document.querySelector<HTMLDivElement>("#app");
-assert(app !== null, "app element not found");
+const app = document.querySelector<HTMLDivElement>('#app');
+assert(app !== null, 'app element not found');
 app.innerHTML = `
   <main>
     <h1>Bomberman</h1>
@@ -14,7 +14,7 @@ app.innerHTML = `
   </main>
 `;
 
-const game = document.querySelector<HTMLCanvasElement>("#game");
-assert(game !== null, "game element not found");
+const game = document.querySelector<HTMLCanvasElement>('#game');
+assert(game !== null, 'game element not found');
 
 setupGame(GridCols, GridRows, game);
--- src/map.ts
+++ src/map.ts
@@ -1,9 +1,9 @@
-import { Pos } from "./pos";
-import { assert } from "./utils";
+import { Pos } from './pos';
+import { assert } from './utils';
 
-type Wall = "W";
-type Walkable = "O";
-type Player = "P";
+type Wall = 'W';
+type Walkable = 'O';
+type Player = 'P';
 
 export type Tile = Wall | Walkable | Player;
 
@@ -13,21 +13,21 @@ export function createMap(
   level: Tile[][],
 ): [Tile[], Pos] {
   let playerPos: Pos | undefined = undefined;
-  assert(height === level.length, "height must match level length");
-  console.log("Creating map...");
+  assert(height === level.length, 'height must match level length');
+  console.log('Creating map...');
   const result: Tile[] = [];
   for (let y = 0; y < height; y++) {
-    assert(level[y].length === width, "width must match level width");
+    assert(level[y].length === width, 'width must match level width');
     for (let x = 0; x < width; x++) {
-      if (level[y][x] === "P") {
-        assert(playerPos === undefined, "only one player allowed");
+      if (level[y][x] === 'P') {
+        assert(playerPos === undefined, 'only one player allowed');
         playerPos = new Pos(x, y);
-        result.push("O");
+        result.push('O');
       } else {
         result.push(level[y][x]);
       }
     }
   }
-  assert(playerPos !== undefined, "no player found");
+  assert(playerPos !== undefined, 'no player found');
   return [result, playerPos];
 }
